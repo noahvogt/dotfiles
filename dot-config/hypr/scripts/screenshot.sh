61f5aaf97c75547a5b84bb6c6be19431158c6751
Noah Vogt <noah@noahvogt.com>
2026-03-26 14:59:17 +0100

    move wl screenshot script from jpg to png + better kitty config

--- dot-config/hypr/scripts/screenshot.sh
+++ dot-config/hypr/scripts/screenshot.sh
@@ -8,19 +8,19 @@ mkdir -p "$SCREENSHOT_DIR"
 
 # Filename format
 TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
-FILENAME="Screenshot_${TIMESTAMP}_hyprland.jpg"
+FILENAME="Screenshot_${TIMESTAMP}_hyprland.png"
 FILE_PATH="${SCREENSHOT_DIR}/${FILENAME}"
 
 # Capture based on argument
 case "$1" in
 area)
     # Select an area using slurp
-    grim -t jpeg -g "$(slurp)" "$FILE_PATH"
+    grim -t png -g "$(slurp)" "$FILE_PATH"
     ;;
 monitor)
     # Active monitor
     MONITOR=$(hyprctl activeworkspace -j | jq -r '.monitor')
-    grim -t jpeg -o "$MONITOR" "$FILE_PATH"
+    grim -t png -o "$MONITOR" "$FILE_PATH"
     ;;
 window)
     # Active window
@@ -31,7 +31,7 @@ window)
     fi
     WINDOW_AT=$(echo "$WINDOW_DATA" | jq -r '.at | join(",")')
     WINDOW_SIZE=$(echo "$WINDOW_DATA" | jq -r '.size | join("x")')
-    grim -t jpeg -g "${WINDOW_AT} ${WINDOW_SIZE}" "$FILE_PATH"
+    grim -t png -g "${WINDOW_AT} ${WINDOW_SIZE}" "$FILE_PATH"
     ;;
 *)
     echo "Usage: $0 {area|monitor|window}"
@@ -41,6 +41,5 @@ esac
 
 # Check if file was created and copy to clipboard
 if [ -f "$FILE_PATH" ]; then
-    wl-copy -t image/jpeg <"$FILE_PATH"
-    notify-send "Screenshot saved" "$FILENAME" -i "$FILE_PATH"
+    wl-copy -t image/png <"$FILE_PATH"
 fi
