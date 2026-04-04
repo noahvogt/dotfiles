62f2707bffaa8e60e651b8325a28abb2e8268cf7
Noah Vogt <noah@noahvogt.com>
2026-04-05 01:25:55 +0200

    simpler waybar + andvanced notification for display brightness change

---
+++ dot-config/hypr/scripts/brightness.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Apply the brightness change passed as an argument (e.g., 5%+ or 5%-)
+brightnessctl -e4 -n2 set "$1"
+
+PERCENT=$(brightnessctl -m | awk -F, '{print $4}' | tr -d '%')
+
+notify-send -i display-brightness-symbolic \
+    -h int:transient:1 \
+    -h int:SWAYNC_BYPASS_DND:1 \
+    -h string:x-canonical-private-synchronous:brightness \
+    -h int:value:"$PERCENT" \
+    -t 1500 \
+    "Brightness: ${PERCENT}%"
